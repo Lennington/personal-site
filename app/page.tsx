469f35379caee7b2de3310a33b46aae9797df3ea
Ian Goode <iandgoode@gmail.com>
2024-01-03 14:07:20 +0000

    Adds prettier for tailwind classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import headshot from '../public/ian-goode-bw.jpg'
 import { Instrument_Serif } from 'next/font/google'
 
 const fontTitle = Instrument_Serif({ 
@@ -13,11 +14,12 @@ export default function Home() {
       <div className="flex flex-col space-y-12 items-start">
         <Image
           className="relative rounded-full max-w-3xl"
-          src="/ian-goode-bw.jpg"
+          src={headshot}
           alt="Photo of Ian Goode"
           width={96}
           height={96}
           priority
+          placeholder="blur"
         />
 
         <h1 className={`${fontTitle.className} text-5xl md:text-8xl max-w-3xl`}>Hey, I’m Ian Goode.</h1>
@@ -35,7 +37,7 @@ export default function Home() {
             <svg xmlns="http://www.w3.org/2000/svg" width="23" height="24" fill="none"><path fill="currentColor" fill-rule="evenodd" d="m22.04 5.386.709 1.215-9.343 5.395 9.343 5.394-.71 1.215-9.334-5.394V24h-1.41V0h1.41v10.78l9.334-5.394ZM6.611 1.257l-1.214.701 3.385 5.863 1.213-.701-3.384-5.863ZM0 11.294h6.77v1.402H0v-1.402ZM7.114 14l-5.862 3.385.7 1.214 5.863-3.385-.7-1.214ZM5.38 22.04l3.385-5.863v.002l1.214.7-3.385 5.863-1.214-.702ZM1.26 6.605l.702-1.215 5.863 3.386-.702 1.213-5.862-3.384Z" clip-rule="evenodd"/></svg>
             Kinzen
           </a>
-          , where I helped global platforms keep their communities safe from harmful content. Kinzen was acquired by Spotify in 2022.
+          , where I worked on software to help analysts identify harmful content at scale. Kinzen was acquired by Spotify in 2022.
         </p>
 
         <p className="md:text-xl max-w-3xl leading-relaxed">Before Kinzen, I led the design team at {' '}
@@ -43,7 +45,7 @@ export default function Home() {
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="23" fill="none"><path fill="#2A34F5" d="M0 0h6.53c.235 0 .411.059.529.235 2.706 3.647 5.47 7.294 8.176 10.942.177.235.118.352 0 .529C12.53 15.353 9.765 19 7.06 22.647c-.118.118-.294.235-.412.235H0c.059-.117.118-.235.176-.294C2.882 19 5.588 15.353 8.294 11.765c.177-.236.177-.412 0-.647C5.588 7.529 2.882 3.882.176.294.118.235.06.176 0 0Zm24 22.941h-6.53c-.235 0-.411-.059-.529-.294a42.13 42.13 0 0 0-1.47-1.941c1.117-1.53 2.294-3.059 3.47-4.588A254.22 254.22 0 0 0 24 22.94ZM18.882 6.824c-1.176-1.53-2.294-3.06-3.47-4.589L17 .118c.059-.06.177-.06.235-.06h6.53c.058 0 .058 0 .117.06a395.002 395.002 0 0 1-5 6.706Z"/></svg>
             Axonista
           </a>
-          , where I worked on a platform to power interactive video experiences, and designed video apps for global brands.
+          , where I worked on a platform to power interactive video experiences and designed video apps for global brands.
         </p>
 
         <a href="mailto:iandgoode@gmail.com" class="flex flex-none basis-auto items-center gap-3 bg-white dark:bg-dark-base focus:outline-none focus:ring-2 focus:ring-gray-600 dark:focus:ring-gray-300 rounded-lg px-4 py-3 text-sm dark:bg-dark-base dark:text-white hover:bg-gray-100 dark:hover:bg-dark-highlight">
