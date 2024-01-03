bc2dc50b1526135ee94ebaaf5d2bae4dc45864f1
Ian Goode <iandgoode@gmail.com>
2024-01-03 14:12:57 +0000

    Removes contact button
    Waiting for domains to hook up

--- app/page.tsx
+++ app/page.tsx
@@ -104,28 +104,6 @@ export default function Home() {
           , where I worked on a platform to power interactive video experiences
           and designed video apps for global brands.
         </p>
-
-        <a
-          href="mailto:iandgoode@gmail.com"
-          class="flex flex-none basis-auto items-center gap-3 rounded-lg bg-white px-4 py-3 text-sm hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-600 dark:bg-dark-base dark:bg-dark-base dark:text-white dark:hover:bg-dark-highlight dark:focus:ring-gray-300"
-        >
-          <svg
-            class="h-[20px] w-[20px] text-gray-800 dark:text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 16"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1.5"
-              d="m19 2-8.4 7.05a1 1 0 0 1-1.2 0L1 2m18 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1m18 0v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2"
-            />
-          </svg>
-          iandgoode@gmail.com
-        </a>
       </div>
     </main>
   );
